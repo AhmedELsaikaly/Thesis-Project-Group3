f271b38ff072729c6086e8a41b56dcac8a6b7a8e
Yousef Yasin <64899736+YousefYasin@users.noreply.github.com>
2020-08-02 19:15:16 +0300

    landPage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { SignIn } from "./Signing/signIn";
+import { SignUp } from "./Signing/signUp";
+import { About } from "./about/about";
+import { Contact } from "./contact/contact";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LandPage from "./landPage/landPage";
+
+import "./App.css";
+
+class App extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={LandPage}></Route>
+            <Route exact path="/signUp" component={SignUp}></Route>
+            <Route exact path="/signIn" component={SignIn}></Route>
+            <Route exact path="/about" component={About}></Route>
+            <Route exact path="/contact" component={Contact}></Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Signing/signIn.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import "./style.css";
+
+class SignIn extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>SIIIIIIGNNNNNN IN</div>
+    );
+  }
+}
+export { SignIn };
