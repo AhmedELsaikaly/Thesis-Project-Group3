d8eee1e1eeccfec36dc00bab90bd5de04aec1c63
waelq <waealtaqia20152729@gmail.com>
2020-08-04 19:10:50 +0300

    server refactor

--- server/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb+srv://Khaled_20:258852@cluster0.gpjac.mongodb.net/test",
-  secretOrKey: "DontTellAnyBody",
-};
---
+++ server/database/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  username : "wael",
+  password : "1234",
+  dbname : "ThesisProject"
+}
