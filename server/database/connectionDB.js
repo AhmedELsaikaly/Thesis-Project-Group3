5a0b5bcae79a329d48584c75e8f37589ca02b137
Khaled-abousheikh <Khaled-abousheikh@gmail.com>
2020-08-05 20:05:30 +0300

    co-dev

--- server/database/connectionDB.js
+++ server/database/connectionDB.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose"); //inclode mongoose in our page
 // const config = require('./config');
 //connection to mongodb
 const db = mongoose
-  .connect(`mongodb://localhost:27017/test11`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://Khaled_20:258852@cluster0.gpjac.mongodb.net/ratest?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log("connected successfully DB");
   })
