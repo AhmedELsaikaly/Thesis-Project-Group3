cd13363ed120c7f783c81f3491fbeebd57c1bdcc
Khaled-abousheikh <Khaled-abousheikh@gmail.com>
2020-08-10 10:52:21 +0300

    dev mergeing

--- client/src/apiMapGoogle/ApiMap.js
+++ client/src/apiMapGoogle/ApiMap.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react'
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+//import used technologies
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
-
-
+//create ApiMap Compo
 export class ApiMap extends Component {
-    render() {
-      return (
-        <Map google={this.props.google} zoom={14}>
-   
-          <Marker onClick={this.onMarkerClick}
-                  name={'Current location'} />
-   
-          <InfoWindow onClose={this.onInfoWindowClose}>
-              <div>
-                {/* <h1>{this.state.selectedPlace.name}</h1> */}
-              </div>
-          </InfoWindow>
-        </Map>
-      );
-    }
+  // render ApiMap
+  render() {
+    return (
+      <Map google={this.props.google} zoom={14}>
+        <Marker onClick={this.onMarkerClick} name={"Current location"} />
+        <InfoWindow onClose={this.onInfoWindowClose}>
+          <div>{/* <h1>{this.state.selectedPlace.name}</h1> */}</div>
+        </InfoWindow>
+      </Map>
+    );
   }
-   
-  export default GoogleApiWrapper({
-    apiKey: ("AIzaSyDTuZu_Pe5uaFt1U-uIwi9tikyHn6swI_A")
-  })(ApiMap)
+}
+
+//export GoogleApiWrapper
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyDTuZu_Pe5uaFt1U-uIwi9tikyHn6swI_A",
+})(ApiMap);
