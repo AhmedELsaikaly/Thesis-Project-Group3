873a0cae7a399a1a5034c597c5de1039664da769
Khaled-abousheikh <Khaled-abousheikh@gmail.com>
2020-08-19 19:58:36 +0300

    dev

--- client/src/Components/ControlPanel/InternalNavControl/InternalNavControl.js
+++ client/src/Components/ControlPanel/InternalNavControl/InternalNavControl.js
@@ -1,10 +1,10 @@
 //import used technologies
-import React from 'react';
-import { Link, withRouter, Redirect } from 'react-router-dom';
-import logo from './Logo.png';
-import { toast } from 'react-toastify';
+import React from "react";
+import { Link, withRouter, Redirect } from "react-router-dom";
+import logo from "./Logo.png";
+import { toast } from "react-toastify";
 //import CSS
-import './InternalNavControl.css';
+import "./InternalNavControl.css";
 
 //import used files
 
@@ -16,53 +16,47 @@ class InternalNav extends React.Component {
     this.state = {};
   }
   logout = () => {
-    toast('Success! Logging out, Thank you❤', { type: 'success' });
-    localStorage.removeItem('usertoken');
+    toast("Success! Logging out, Thank you❤", { type: "success" });
+    localStorage.removeItem("ownertoken");
   };
   render() {
     console.log(this.props);
     return (
-      <section id='nav-bar'>
+      <section id="nav-bar">
         <nav
-          style={{ marginBottom: '-10px' }}
-          className='navbar navbar-expand-lg navbar-light'
+          style={{ marginBottom: "-10px" }}
+          className="navbar navbar-expand-lg navbar-light"
         >
-          <a className='navbar-brand' href='#'>
+          <a className="navbar-brand" href="#">
             <img src={logo} />
           </a>
           <h1>Raha</h1>
           <button
-            className='navbar-toggler'
-            type='button'
-            data-toggle='collapse'
-            data-target='#navbarNav'
-            aria-controls='navbarNav'
-            aria-expanded='false'
-            aria-label='Toggle navigation'
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
-            <span className='navbar-toggler-icon'></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div className='collapse navbar-collapse' id='navbarNav'>
-            <ul className='navbar-nav ml-auto'>
-              <li className='nav-item'>
-                <a className='nav-link' href='/'>
-                  HOME
-                </a>
-              </li>
-
-              <li className='nav-item'>
-                <a className='nav-link' href='/contact'>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/contact">
                   CONTACT
                 </a>
               </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='/about'>
+              <li className="nav-item">
+                <a className="nav-link" href="/about">
                   ABOUT US
                 </a>
               </li>
 
-              <li className='nav-item'>
-                <a className='nav-link' href='/' onClick={this.logout}>
+              <li className="nav-item">
+                <a className="nav-link" href="/" onClick={this.logout}>
                   LOGOUT
                 </a>
               </li>
--- client/src/Components/userPage/resort.js
+++ client/src/Components/userPage/resort.js
@@ -1,21 +1,4 @@
 //import used technologies
-<<<<<<< HEAD
-import React from 'react';
-import './Resort.css';
-import './styles.css';
-import './facility.css';
-import axios from 'axios';
-import Comment from '../Rating&Feedback/Comments';
-import Reservation from './reservation';
-import Pay from './pay';
-import one from './1.jpg';
-import two from './2.jpg';
-import three from './3.jpg';
-import { NotEditStar } from './../Rating&Feedback/Rating';
-import Footer from './Footer';
-import './Footer.css';
-import InternalNav from './../InternalNav/InternalNav';
-=======
 import React from "react";
 import "./Resort.css";
 import "./styles.css";
@@ -32,7 +15,6 @@ import { NotEditStar } from "./../Rating&Feedback/Rating";
 import Footer from "./Footer";
 import "./Footer.css";
 import InternalNav from "./../InternalNav/InternalNav";
->>>>>>> 0414bedac45bef26da59ea8aa030874691122a4c
 
 //import used files
 
@@ -44,14 +26,14 @@ class Facility extends React.Component {
       <div>
         {Object.keys(this.props.faclitics).length !== 0 ? (
           <div>
-            <div className='pack'>
+            <div className="pack">
               <h1>Our facility</h1>
             </div>
-            <section id='packages'>
+            <section id="packages">
               {Object.keys(this.props.faclitics).map((element, index) => (
-                <div className='all'>
-                  <div class='box'>
-                    <div class='content2'>
+                <div className="all">
+                  <div class="box">
+                    <div class="content2">
                       <div>
                         <h2>{element}</h2>
                         <h3>
@@ -83,10 +65,10 @@ class Resort extends React.Component {
     this.state = {
       service: [],
       faclitics: {},
-      ownerId: '',
+      ownerId: "",
       isFull: false,
-      owner: '',
-      selectedFacility: '',
+      owner: "",
+      selectedFacility: "",
       SelectedPrice: 0,
     };
   }
@@ -118,7 +100,7 @@ class Resort extends React.Component {
           });
         })
         .catch((err) => {
-          console.log('ERROR from AXIOS =>', err);
+          console.log("ERROR from AXIOS =>", err);
         });
       axios
         .get(`http://localhost:5000/Facilites/${this.state.ownerId}`)
@@ -126,7 +108,7 @@ class Resort extends React.Component {
           this.setState({ faclitics: res.data[0].facilities });
         })
         .catch((err) => {
-          console.log('ERROR from AXIOS =>', err);
+          console.log("ERROR from AXIOS =>", err);
         });
       axios
         .get(`http://localhost:5000/Owner/${this.state.ownerId}`)
@@ -135,7 +117,7 @@ class Resort extends React.Component {
           this.setState({ owner: res.data[0] });
         })
         .catch((err) => {
-          console.log('ERROR from AXIOS =>', err);
+          console.log("ERROR from AXIOS =>", err);
         });
     }
   }
@@ -146,47 +128,43 @@ class Resort extends React.Component {
     const { service } = this.state;
 
     return (
-<<<<<<< HEAD
-      <div style={{ width: '100%' }}>
-=======
       <div style={{ width: "100%" }}>
->>>>>>> 0414bedac45bef26da59ea8aa030874691122a4c
         <div>
           <InternalNav />
         </div>
         {/* Slider owner */}
-        <div id='slider' className='carousel slide' data-ride='carousel'>
-          <div className='carousel-inner'>
-            <div className='carousel-item active'>
-              <img src={one} className='d-block w-100' alt='slide1' />
-              <div id='caption'>
+        <div id="slider" className="carousel slide" data-ride="carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={one} className="d-block w-100" alt="slide1" />
+              <div id="caption">
                 <h1>welcome to {this.state.owner.placeName}</h1>
-                <div className='blured'></div>
+                <div className="blured"></div>
               </div>
             </div>
-            <div className='carousel-item'>
-              <img src={two} className='d-block w-100' alt='slide2' />
-              <div id='caption'>
+            <div className="carousel-item">
+              <img src={two} className="d-block w-100" alt="slide2" />
+              <div id="caption">
                 <h1>{this.state.owner.placeName}</h1>
-                <div className='blured'></div>
+                <div className="blured"></div>
               </div>
             </div>
-            <div className='carousel-item'>
-              <img src={three} className='d-block w-100' alt='slide3' />
-              <div id='caption'>
+            <div className="carousel-item">
+              <img src={three} className="d-block w-100" alt="slide3" />
+              <div id="caption">
                 <h1>{this.state.owner.placeName}</h1>
-                <div className='blured'></div>
+                <div className="blured"></div>
               </div>
             </div>
           </div>
         </div>
         {/* ******************************** */}
         {/* *************Owner Information************** */}
-        <div id='gar' class='garden3'>
-          <div class='imgBx2'>
+        <div id="gar" class="garden3">
+          <div class="imgBx2">
             <img src={this.state.owner.licensePhoto} />
           </div>
-          <div class='content4'>
+          <div class="content4">
             <h1>{this.state.owner.placeName}</h1>
             <p>rating</p>
             <NotEditStar rate={this.state.owner.ratingAvg} />
@@ -194,59 +172,51 @@ class Resort extends React.Component {
               We work to provide everything necessary to ensure your comfort and
               enjoy your trip in our facilities
             </p>
-            <h5 class='features'>Services</h5>
+            <h5 class="features">Services</h5>
             <ul>
               {service.map((dataIN, key) => (
-                <li style={{ width: '400px', height: '50px' }}>{dataIN}</li>
+                <li style={{ width: "400px", height: "50px" }}>{dataIN}</li>
               ))}
             </ul>
 
             <button
-<<<<<<< HEAD
-              type='button'
-              class='btn btn-info pull-right'
-              data-toggle='modal'
-              data-target='#exampleModal'
-              data-whatever='@mdo'
-=======
               type="button"
               class="btn btn-info pull-right"
               data-toggle="modal"
               data-target="#exampleModal"
               data-whatever="@mdo"
->>>>>>> 0414bedac45bef26da59ea8aa030874691122a4c
             >
               Book Now
             </button>
 
             <div
-              class='modal fade'
-              id='exampleModal'
-              tabindex='-1'
-              role='dialog'
-              aria-labelledby='exampleModalLabel'
-              aria-hidden='true'
+              class="modal fade"
+              id="exampleModal"
+              tabindex="-1"
+              role="dialog"
+              aria-labelledby="exampleModalLabel"
+              aria-hidden="true"
             >
-              <div class='modal-dialog' role='document'>
-                <div class='modal-content'>
-                  <div class='modal-header'>
-                    <h5 class='modal-title' id='exampleModalLabel'>
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">
                       Booking facility
                     </h5>
                     <button
-                      type='button'
-                      class='close '
-                      id='cl'
-                      data-dismiss='modal'
-                      aria-label='Close'
+                      type="button"
+                      class="close "
+                      id="cl"
+                      data-dismiss="modal"
+                      aria-label="Close"
                     >
-                      <span aria-hidden='true'>&times;</span>
+                      <span aria-hidden="true">&times;</span>
                     </button>
                   </div>
-                  <div class='modal-body'>
+                  <div class="modal-body">
                     <form>
-                      <div class='form-group'></div>
-                      <div class='form-group'>
+                      <div class="form-group"></div>
+                      <div class="form-group">
                         <Reservation
                           handleSelection={this.handleSelection}
                           ownerId={this.state.ownerId}
@@ -255,7 +225,7 @@ class Resort extends React.Component {
                       </div>
                     </form>
                   </div>
-                  <div class='modal-footer'>
+                  <div class="modal-footer">
                     {/* **************Pay ******************** */}
                     <Pay
                       description={this.state.selectedFacility}
@@ -274,12 +244,12 @@ class Resort extends React.Component {
           </div>
         </div>
         {/* *************faclitics***************** */}
-        <div className='facility'>
+        <div className="facility">
           <Facility faclitics={this.state.faclitics} />
         </div>
         {/* <Reservation ownerId={this.state.ownerId} /> */}
         {/* <Pay /> */}
-        <div className='rating'>
+        <div className="rating">
           {/* *************Comment ***************** */}
 
           <Comment OwnerId={this.state.ownerId} />
